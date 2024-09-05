9d2014610022eda702fb3fbe094b94539389ed82
AnkitKumar <roy.ankit5008@gmail.com>
2024-09-05 23:48:05 +0530

    added allowed host

--- config/settings/development.py
+++ config/settings/development.py
@@ -2,7 +2,7 @@ from .base import *
 
 
 DEBUG = True
-ALLOWED_HOSTS = ['localhost','127.0.0.1',]
+ALLOWED_HOSTS = ['localhost','127.0.0.1', 'recipe-api-4ytq.onrender.com',]
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
